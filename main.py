08d601397e73212c64a415a7cc72b2e0dc0fd2cf
Cristian Mirea <cristian.mirea.mc@gmail.com>
2024-08-11 16:29:24 +0300

    Auto-check for files

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import time
+
+
+directoryPath = r'C:\Users\CristianMirea\OneDrive - RightClick Solutions, B.V\Desktop\SQL\Python\Leasons\MainProject\MainProject\Intrari'
+def FirstTry():
+    if  not os.listdir(directoryPath):
+        print('There are no files. Auto-Checking after 1 min ')
+        TimeCheck()
+    else:
+        print('Files were found in the folder. Uploading the data.')
+
+def TimeCheck():
+    timetocheck=0
+    while timetocheck<10:
+        time.sleep(1)
+        timetocheck+=1
+    else:
+        timetocheck=0
+        CheckingFileExist()
+        
+        
+def CheckingFileExist (): 
+    if  not os.listdir(directoryPath):
+        print('There are no files. Auto-Checking after 1 min ')
+        TimeCheck()
+    else:
+        print('Files were found in the folder. Uploading the data.')
+
+
+FirstTry()
+
+
+
+        
+
